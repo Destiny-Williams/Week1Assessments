ad2a138501f4bb32992c06cd1dbb711056039660
Destiny-Williams <bishde5v@gmail.com>
2021-11-01 15:03:54 -0500

    test

---
+++ gitInfo.js
@@ -0,0 +1,89 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+
+let gitDefinition = "To me Git it an way to explain that a code/file is avialbe and open to retrive."
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+
+let gitHubDefinition = "GitHub is a web-based interface that uses Git, the open source version control software that lets multiple people make separate changes to web pages at the same time.";
+//////////////////PROBLEMS 3 - 8////////////////////
+/*
+    For the next several problems you will be creating objects containing information about different git 
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
+    will be a string with a description of what that git command does.  The 'code' property should be a 
+    string of the actual command (what you type into your terminal).
+*/
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create an object called 'init' with 'description' and 'code' properties 
+    following the guidelines above to describe the init command.
+*/
+
+//CODE HERE
+
+let init = "initating an action."
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create an object called 'clone' with 'description' and 'code' properties 
+    following the guidelines above to describe the clone command.
+*/
+
+//CODE HERE
+
+let clone = "clone is when you make a copy of file/code, and prep it to run"
+//////////////////PROBLEM 5////////////////////
+/*
+    Create an object called 'status' with 'description' and 'code' properties 
+    following the guidelines above to describe the status command.
+*/
+
+//CODE HERE
+
+let status = "status helps us figure out the curent state for our file/code, like where and what it is preping for"
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create an object called 'add' with 'description' and 'code' properties 
+    following the guidelines above to describe the add command.
+*/
+
+//CODE HERE
+
+let add = "add means we will be inputting/updating the selected file/code"
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create an object called 'commit' with 'description' and 'code' properties
+    following the guidelines above to describe the commit command.
+*/
+
+//CODE HERE
+let commit = "commit is us saying yes to the action we were taking steps to make, and getting our file ready to push."
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create an object called 'push' with 'description' and 'code' properties 
+    following the guidelines above to describe the push command.
+*/
+
+//CODE HERE
+
+let push = "push is our final step in us commiting a file to a share able drive, or our local computer. "
---
+++ hello.js
@@ -0,0 +1,7 @@
+console.log("You're ready to begin!");
+
+
+
+// NOTE FORM STUDENT:
+//I am sorry i did not have access to the insturctions for hello.js. 
+// 
